1c43e307f2d2e95dba836afb20d31811b9a1fb0f
Swimming Potato <ptphong200i@gmail.com>
2022-03-24 13:23:44 +0700

    Add hover effect

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 for (let i = 0; i < 16*16; i++) {
     const container = document.getElementById("container");
     const grid = document.createElement("div");
-
+    grid.classList.add("grid")
+    grid.addEventListener("mouseover", () => {
+        grid.setAttribute("style", "background-color: black;");
+    })
     container.appendChild(grid);
 }
