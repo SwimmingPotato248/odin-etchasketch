de79c6aeab7364eac693f737b71cdeefafacd863
Swimming Potato <ptphong200i@gmail.com>
2022-03-24 12:49:57 +0700

    Create the grid

---
+++ javascript.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 16*16; i++) {
+    const container = document.getElementById("container");
+    const grid = document.createElement("div");
+
+    container.appendChild(grid);
+}
